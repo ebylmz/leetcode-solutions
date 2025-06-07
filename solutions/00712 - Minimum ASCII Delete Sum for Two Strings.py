0fcd3c9a61da43892ef5f4dd96cbfd51b3ae5c7e
ebylmz <emirkan.b.yilmaz@gmail.com>
2025-06-07 12:00:44 +0300

    bottom up approach 712

--- solutions/00712 - Minimum ASCII Delete Sum for Two Strings.py
+++ solutions/00712 - Minimum ASCII Delete Sum for Two Strings.py
@@ -1,28 +1,36 @@
 class Solution(object):
     def minimumDeleteSum(self, s1, s2):
         """
-        :type s1: str
-        :type s2: str
-        :rtype: int
+            Time Complexity: O(m * n), where m = len(s1) and n = len(s2)
+            Space Complexity: O(m * n) 
         """
-        dp = [[None] * len(s2) for _ in range(len(s1))]
 
-        def bfs(i, j):
-            if i == len(s1):
-                return sum(ord(c) for c in s2[j:])
-            if j == len(s2):
-                return sum(ord(c) for c in s1[i:])
-            
-            if dp[i][j] is not None:
-                return dp[i][j]
+        m, n = len(s1), len(s2) 
+        dp = [[0] * (n + 1) for _ in range(m + 1)] # matrix: (m + 1) x (n + 1) 
 
-            if s1[i] == s2[j]:
-                dp[i][j] = bfs(i + 1, j + 1)
-            else:
-                c1 = ord(s1[i]) + bfs(i + 1, j)
-                c2 = ord(s2[j]) + bfs(i, j + 1)
-                dp[i][j] = min(c1, c2)
+        for i in range(m - 1, -1, -1):
+            dp[i][n] = dp[i+1][n] + ord(s1[i])
+        for j in range(n - 1, -1, -1):
+            dp[m][j] = dp[m][j+1] + ord(s2[j])
+        
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if s1[i] == s2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = min(
+                        dp[i+1][j] + ord(s1[i]),
+                        dp[i][j+1] + ord(s2[j])
+                    )
+        return dp[0][0]
 
-            return dp[i][j]
-
-        return bfs(0, 0)
+"""
+                                   (n)
+            0   1   2   3   4   5   6
+            d   e   l   e   t   e   \
+    0   l   0   .   .   .   .   .   .
+    1   e   .   .   .   .   .   .   .
+    2   e   .   .   .   .   .   .   .
+    3   t   .   .   .   .   .   .   t
+(m) 4   \   .   .   .   .   .   e   0
+"""
